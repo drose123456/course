c06f1cda5512b5c8dfe7cb244be5931c559272da
drose123456 <acg_danial@hotmail.com>
2020-02-05 20:42:47 +0000

    model for courses

---
+++ models/courses.js
@@ -0,0 +1,10 @@
+let mongoose = require("mongoose");
+
+let courseSchema = new mongoose.Schema({
+  courseName: String,
+  coursePrice: Number,
+  courseProvider: String,
+  courseReviews: []
+});
+
+module.exports = mongoose.model("Course", courseSchema);
